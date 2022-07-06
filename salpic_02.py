6b6de680c911681d6dafe49d635db64a1b0eb811
burningVic <88430326+burningVic@users.noreply.github.com>
2022-07-06 14:27:31 +0200

    Update salpic_02.py

--- salpic_02.py
+++ salpic_02.py
@@ -199,9 +199,9 @@ class SalpicDataset(utils.Dataset):
         image_dir = "{}/{}{}".format(dataset_dir, subset, year)
 
         # Load all classes or a subset?
-        if not class_ids:
+        #if not class_ids:
             # All classes
-            class_ids = sorted(coco.getCatIds())
+            #class_ids = sorted(coco.getCatIds())
 
         # All images or a subset?
         if class_ids:
